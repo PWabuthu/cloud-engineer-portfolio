fcb04c80f76ba37b5e01bd72a0e15a7ff978f668
PWabuthu <patrickwabuthu@gmail.com>
2026-02-12 23:06:21 -0500

    Add Internet Gateway and public routing

--- 01-terraform-aws-foundation/terraform/main.tf
+++ 01-terraform-aws-foundation/terraform/main.tf
@@ -24,3 +24,29 @@ resource "aws_subnet" "private" {
     Name = "${var.project_name}-private-subnet"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.project1.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.project1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
