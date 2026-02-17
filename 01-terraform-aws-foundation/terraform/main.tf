949e88e832d4157386355aef19f5945a5bc68bf3
PWabuthu <patrickwabuthu@gmail.com>
2026-02-16 22:12:39 -0500

    Add security groups for bastion and private instances

--- 01-terraform-aws-foundation/terraform/main.tf
+++ 01-terraform-aws-foundation/terraform/main.tf
@@ -50,3 +50,53 @@ resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_security_group" "bastion_sg" {
+  name        = "${var.project_name}-bastion-sg"
+  description = "Allow SSH from trusted IP"
+  vpc_id      = aws_vpc.project1.id
+
+  ingress {
+    description = "SSH from trusted IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_allowed_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-bastion-sg"
+  }
+}
+
+resource "aws_security_group" "private_sg" {
+  name        = "${var.project_name}-private-sg"
+  description = "Allow SSH only from bastion"
+  vpc_id      = aws_vpc.project1.id
+
+  ingress {
+    description     = "SSH from bastion SG"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-private-sg"
+  }
+}
