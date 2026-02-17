abafa5aeac78189986aeb564712f783255705407
PWabuthu <patrickwabuthu@gmail.com>
2026-02-16 22:35:12 -0500

    Add EC2 bastion and private instances

---
+++ 01-terraform-aws-foundation/terraform/ec2.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "bastion" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
+  key_name                    = var.key_name
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "${var.project_name}-bastion"
+  }
+}
+
+resource "aws_instance" "private" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.private.id
+  vpc_security_group_ids = [aws_security_group.private_sg.id]
+  key_name               = var.key_name
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+
+  tags = {
+    Name = "${var.project_name}-private"
+  }
+}
